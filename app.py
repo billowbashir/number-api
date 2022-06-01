4360fa6e2dfbee1ac4f48c043b4377b778bb3274
billowbashir <billowbashir@gmail.com>
2022-06-01 17:53:42 +0300

    make endpoints

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
- import flask
+from flask import Flask, json
 
- # app setup
- app = Flask(__name__)
+# app setup
+app = Flask(__name__)
+@app.route("/odds",methods=["GET"])
+def get_odds():
+    odd_numbers = [1,3,5,7,9]
+    return json.dumps(odd_numbers)
